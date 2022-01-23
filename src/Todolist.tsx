c02c4e98f9fc83b5050627ddcc1b4d76d5c2d72c
89041088851 <bogdan.kosnyrev@outlook.com>
2022-01-23 15:20:36 +0300

    added project todo list

---
+++ src/Todolist.tsx
@@ -0,0 +1,72 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {FilterValuesType} from "./App";
+
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+  removeTask: (taskId: string) => void
+  addTask: (title: string) => void
+  changeFilter: (value: FilterValuesType) => void
+}
+
+export type TaskType = {
+  id: string
+  title: string
+  isDone: boolean
+}
+
+export const Todolist = (props: PropsType) => {
+  let [title, setTitle] = useState('')
+  let addTask = () => {
+    props.addTask(title)
+    setTitle('')
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+  }
+
+  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.charCode === 13) {
+      addTask()
+    }
+  }
+
+  const onAllClickHandler = () => props.changeFilter('all')
+  const onActiveClickHandler = () => props.changeFilter('active')
+  const onCompletedClickHandler = () => props.changeFilter('completed')
+
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input
+          value={title}
+          onChange={onChangeHandler}
+          onKeyPress={onKeyPressHandler}
+        />
+        <button onClick={addTask}>+</button>
+      </div>
+      <ul>
+        {
+          props.tasks.map(t => {
+            const onClickHandler = () => props.removeTask(t.id)
+              return (
+                <li key={t.id}>
+                  <input type="checkbox" checked={t.isDone}/>
+                  <span>{t.title}</span>
+                  <button onClick={onClickHandler}>x</button>
+                </li>
+              )
+            }
+          )
+        }
+      </ul>
+      <div>
+        <button onClick={onAllClickHandler}>All</button>
+        <button onClick={onActiveClickHandler}>Active</button>
+        <button onClick={onCompletedClickHandler}>Completed</button>
+      </div>
+    </div>
+  )
+}
